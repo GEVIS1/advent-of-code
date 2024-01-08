e8f0dfacdb994be87a1845f76b852e619d3c7acc
Steffen Geving <79884124+GEVIS1@users.noreply.github.com>
2024-01-08 19:22:37 +0000

    feat: add result function to Hand class

--- src/2023/python/7/main.py
+++ src/2023/python/7/main.py
@@ -1,10 +1,9 @@
 import os
 import re
+from collections import Counter
 
 Rank = str
-
-class Card:
-    rank_to_value = {
+rank_to_value = {
         "1": 1,
         "2": 2,
         "3": 3,
@@ -20,6 +19,18 @@ class Card:
         "K": 13,
         "A": 14
 }
+
+hand_result_precedence = [
+    "five_of_a_kind",
+    "four_of_a_kind",
+    "full_house",
+    "three_of_a_kind",
+    "two_pairs",
+    "one_pair",
+    "high_card",
+]
+
+class Card:
     def __init__(self, r: Rank = 'A'):
         self.rank = self.__parse__(r)
 
@@ -36,10 +47,10 @@ class Card:
         b = other.rank
 
         if not a.isdigit():
-            a = self.rank_to_value[a]
+            a = rank_to_value[a]
 
         if not b.isdigit():
-            b = self.rank_to_value[b]
+            b = rank_to_value[b]
 
         return int(a) > int(b)
     
@@ -52,40 +63,73 @@ class Hand:
         self.hand = self.__parse__(hand)
 
     def __parse__(self, hand: str):
-        
         if len(hand) != 5:
             raise ValueError("Hand must be 5 cards long")
 
         for c in hand:
-            Card(c)    
+            Card(c)
             
         return hand.upper()
 
-    def is_pair(self, checked_rank = None):
-        # Todo: Remove need for card instance
-        c = Card()
+    def full_house(self, hand_counter: Counter):
+        values = hand_counter.values()
+        return 3 in values and 2 in values
 
-        checked_rank = str(checked_rank)
-        
-        hand_list = sorted(list(self.hand))
-
-        if checked_rank:
-            c = Card(checked_rank)
-            if hand_list.count(checked_rank) == 2:
-                return True
-        else:
-            for rank in c.rank_to_value.keys():
-                if hand_list.count(rank) == 2:
-                    return True
-            
-        return False
-        
-        
-
-
-def decide_stronger_hand(a: Hand, b: Hand) -> Hand:
+    def five_of_a_kind(self, hand_counter: Counter):
+        values = hand_counter.values()
+        return 5 in values
     
-    pass
+    def four_of_a_kind(self, hand_counter: Counter):
+        values = hand_counter.values()
+        return 4 in values
+    
+    def three_of_a_kind(self, hand_counter: Counter):
+        values = hand_counter.values()
+        return 3 in values
+    
+    def two_pairs(self, hand_counter: Counter):
+        values = hand_counter.values()
+        return Counter(values)[2] == 2
+    
+    def one_pair(self, hand_counter: Counter):
+        values = hand_counter.values()
+        return 2 in values
+    
+    def high_card(self, hand_counter: Counter = None):
+        if not hand_counter:
+            hand_counter = Counter(list(self.hand))
+        
+        max_card_value = 0
+        max_card_rank = None
+
+        for card in hand_counter.keys():
+            value = rank_to_value[card]
+            if value > max_card_value:
+                max_card_value, max_card_rank = value, card
+
+        return Card(max_card_rank)
+
+    def result(self):
+        hand_counter = Counter(list(self.hand))
+
+        for possible_result in hand_result_precedence:
+            result_function = getattr(self, possible_result)
+            if result_function(hand_counter):
+                return possible_result
+        
+        return None
+    
+    def __gt__(self, other):
+        a = self.result()
+        b = self.result()
+        
+        if a == b:
+            return Card(self.hand[0]) > Card(other.hand[0])
+        
+        return hand_result_precedence.index(a) < hand_result_precedence.index(b)
+
+    def __lt__(self, other):
+        return not self.__gt__(other)
 
 if __name__ == "__main__":
     
@@ -94,7 +138,15 @@ if __name__ == "__main__":
         part1_test_input = f.read()
         part1_test_input = list(filter(''.__ne__, part1_test_input.split('\n')))
 
-    print(f"{Hand('12344').is_pair(4) = }")
+    print(f"Five of a kind: {Hand('AAAAA').result()}")
+    print(f"Four of a kind: {Hand('AA8AA').result()}")
+    print(f"Full house: {Hand('23332').result()}")
+    print(f"Three of a kind: {Hand('TTT98').result()}")
+    print(f"Two pair: {Hand('23432').result()}")
+    print(f"One pair: {Hand('A23A4').result()}")
+    print(f"High card: {Hand('23456').result()}")
+
+    print(f"{Hand('T5555') > Hand('J5555') = }")
 
     part1_test_correct_result = 6440
 
