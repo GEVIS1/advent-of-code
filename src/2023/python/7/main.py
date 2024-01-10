9e64a08052d5380eccaa12f9131be1e3b2124d75
Steffen Geving <79884124+GEVIS1@users.noreply.github.com>
2024-01-10 15:34:30 +0000

    feat: add work on part 2

--- src/2023/python/7/main.py
+++ src/2023/python/7/main.py
@@ -1,26 +1,15 @@
 import os
 import re
 from collections import Counter
-from functools import cmp_to_key
+
+def load_input(path: str) -> str:
+    with open(os.path.dirname(__file__) + "/" + path, encoding='utf-8') as f:
+        file_input = f.read()
+        file_input = list(filter(''.__ne__, file_input.split('\n')))
+    
+    return file_input
 
 Rank = str
-rank_to_value = {
-        "1": 1,
-        "2": 2,
-        "3": 3,
-        "4": 4,
-        "5": 5,
-        "6": 6,
-        "7": 7,
-        "8": 8,
-        "9": 9,
-        "T": 10,
-        "J": 11,
-        "Q": 12,
-        "K": 13,
-        "A": 14
-}
-
 hand_result_precedence = [
     "five_of_a_kind",
     "four_of_a_kind",
@@ -48,10 +37,10 @@ class Card:
         b = other.rank
 
         if not a.isdigit():
-            a = rank_to_value[a]
+            a = self.rank_to_value[a]
 
         if not b.isdigit():
-            b = rank_to_value[b]
+            b = self.rank_to_value[b]
 
         return int(a) > int(b)
     
@@ -60,8 +49,55 @@ class Card:
         
 
 class Hand:
-    def __init__(self, hand: str):
+    modes = {
+        "part1": 1,
+        "part2": 2,
+    }
+    part1_rank_to_value = {
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "T": 10,
+        "J": 11,
+        "Q": 12,
+        "K": 13,
+        "A": 14
+    }
+    part2_rank_to_value = {
+        "J": 0,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+        "T": 10,
+        "Q": 12,
+        "K": 13,
+        "A": 14
+}
+
+    def __init__(self, hand: str, bid: int, mode="part1"):
+        self.mode = self.verify_mode(mode)
+        self.rank_to_value = self.part1_rank_to_value if self.mode == "part1" else self.part2_rank_to_value
         self.hand = self.__parse__(hand)
+        self.bid = bid
+
+    # This seems unnecessary
+    def verify_mode(self, mode: str):
+        if mode not in self.modes:
+            raise ValueError("Incorrect mode selection.")
+
+        return mode
 
     def __parse__(self, hand: str):
         if len(hand) != 5:
@@ -104,7 +140,7 @@ class Hand:
         max_card_rank = None
 
         for card in hand_counter.keys():
-            value = rank_to_value[card]
+            value = self.rank_to_value[card]
             if value > max_card_value:
                 max_card_value, max_card_rank = value, card
 
@@ -115,7 +151,20 @@ class Hand:
 
         for possible_result in hand_result_precedence:
             result_function = getattr(self, possible_result)
-            if result_function(hand_counter):
+
+            # For part 2 convert the jack to achieve better result
+            if self.mode == "part2" and "J" in hand_counter:
+                # This is naive and should be tested properly
+                most_represented_card, _  = max(hand_counter.items(), key=lambda i: i[1])
+                jokered_hand_counter = hand_counter.copy()
+                jokered_hand_counter[most_represented_card] += 1
+                del jokered_hand_counter["J"]
+
+                print(f"Joker rule triggered:\n{hand_counter=}\n{jokered_hand_counter=}")
+                if result_function(jokered_hand_counter):
+                    return possible_result
+
+            elif result_function(hand_counter):
                 return possible_result
         
         return None
@@ -126,89 +175,81 @@ class Hand:
         
         # Tie-breaker
         if a == b:
-            print(f"a ({self.hand}) ({a}) is the same as b ({other.hand}) ({b})")
+
             card_index = 0
-            hand_a_card = rank_to_value[self.hand[card_index]]
-            hand_b_card = rank_to_value[other.hand[card_index]]
-            print(f"Hand A card {card_index}: {hand_a_card}. Hand B card {card_index}: {hand_b_card}")
+            hand_a_card = self.rank_to_value[self.hand[card_index]]
+            hand_b_card = self.rank_to_value[other.hand[card_index]]
+
             while hand_a_card == hand_b_card:
-                print("Top of while loop:")
+    
                 card_index += 1
-                hand_a_card = rank_to_value[self.hand[card_index]]
-                hand_b_card = rank_to_value[other.hand[card_index]]
-                print(f"Hand A card {card_index}: {hand_a_card}. Hand B card {card_index}: {hand_b_card}")
-                
-            print(f"{hand_a_card=}\n{hand_b_card=}")
-            print(f"Returning {hand_a_card > hand_b_card =}")
+                hand_a_card = self.rank_to_value[self.hand[card_index]]
+                hand_b_card = self.rank_to_value[other.hand[card_index]]
+    
             return hand_a_card > hand_b_card
         
-        print(f"{hand_result_precedence.index(a) < hand_result_precedence.index(b) = }")
 
         return hand_result_precedence.index(a) < hand_result_precedence.index(b)
 
     def __lt__(self, other):
         return not self.__gt__(other)
+    
+    def __eq__(self, other):
+        sort_fn = lambda s: "".join(sorted(s))
 
-def sort_hands(hands: list[Hand]) -> list[Hand]:
-    hands_are_sorted = False
-    sorted_hands = hands.copy()
-    len_list = len(sorted_hands)
+        a = sort_fn(self.hand)
+        b = sort_fn(other.hand)
 
-    while not hands_are_sorted:
-        print("Top of while sorted loop")
-        hands_are_sorted = True
-        
-        for i in range(len_list):
-            print(f"Top of range loop on i = {i}")
-            end_of_list = i == len_list - 1
-            print(f"{end_of_list=}")
-            if not end_of_list and sorted_hands[i] > sorted_hands[i+1]:
-                hands_are_sorted = False
-                print(f"Swapping {sorted_hands[i]} with {sorted_hands[i+1]}")
-                sorted_hands[i], sorted_hands[i+1] = sorted_hands[i+1], sorted_hands[i]
+        return a == b
 
-    return sorted_hands
+    def __repr__(self):
+        return f"\'{self.hand}\'"
+
+def parse_input(input_str: list[str], mode="part1") -> list[Hand]:
+    hands = []
+
+    for line in input_str:
+        [hand, bid] = line.split(' ')
+        hands.append(Hand(hand, int(bid), mode))
+    
+    return hands
+
+def calculate_sum(sorted_hands: list[Hand]):
+    return sum(h.bid * (i + 1) for i,h in enumerate(sorted_hands))
 
 if __name__ == "__main__":
     
     # Part 1 test
-    with open(os.path.dirname(__file__) + '/test.txt', encoding='utf-8') as f:
-        part1_test_input = f.read()
-        part1_test_input = list(filter(''.__ne__, part1_test_input.split('\n')))
-
+    part1_test_input = load_input('./test.txt')
     part1_test_correct_result = 6440
 
     # part1_test_races = part1_parse_input(part1_test_input)
     # part1_test_answer = part1_solve_races(part1_test_races)
 
-    part1_test_hands = []
-    part1_test_bids = []
+    part1_test_hands = parse_input(part1_test_input, "part1")
+    part1_test_hands_sorted = sorted(part1_test_hands)
 
-    for line in part1_test_input:
-        [hand, bid] = line.split(' ')
-        part1_test_hands.append(hand)
-        part1_test_bids.append(bid)
+    part1_test_answer = calculate_sum(part1_test_hands_sorted)
 
-    part1_test_hands_sorted = sort_hands(part1_test_hands)
+    assert part1_test_answer == part1_test_correct_result, \
+        f"Part 1 test answer was ({part1_test_answer}) " + \
+            f"where it should be ({part1_test_correct_result})"
 
-    print(f"part1_test_hands_sorted=\t{part1_test_hands_sorted}")
+    # Part 1 solution
+    part1_solution_input = load_input('./input.txt')
+    part1_solution_hands = parse_input(part1_solution_input, "part1")
+    part1_solution_hands_sorted = sorted(part1_solution_hands)
+    part1_solution_answer = calculate_sum(part1_test_hands_sorted)
 
-    correct_order = [
-        "32T3K",
-        "KTJJT",
-        "KK677",
-        "T55J5",
-        "QQQJA", 
-    ]
+    print(f"Part 1 solution found: {part1_solution_answer}")
 
-    print(f"correct_order=\t\t\t{correct_order}")
+    # Part 2 test
+    part2_test_correct_result = 5905
+    part2_test_input = load_input('./test.txt')
+    part2_test_hands = parse_input(part2_test_input, "part2")
+    part2_test_hands_sorted = sorted(part2_test_hands)
+    part2_test_answer = calculate_sum(part2_test_hands_sorted)
 
-    # print(f"{Hand('32T3K') < Hand('KTJJT')=}")
-    # print(f"{Hand('KTJJT') < Hand('KK677')=}")
-    # print(f"{Hand('KK677') < Hand('T55J5')=}")
-    # print(f"{Hand('T55J5') < Hand('QQQJA')=}")
-    # print(f"{Hand('QQQJA') > Hand('T55J5')=}") 
-    # print(f"{Hand('KTJJT') > Hand('KK677')=}")
-    # assert part1_test_answer == part1_test_correct_result, \
-    #     f"Part 1 test answer was ({part1_test_answer}) " + \
-    #         f"where it should be ({part1_test_correct_result})"
+    assert part2_test_answer == part2_test_correct_result, \
+        f"Part 2 test answer was ({part2_test_answer}) " + \
+            f"where it should be ({part2_test_correct_result})"
