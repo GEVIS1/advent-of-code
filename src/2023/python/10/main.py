b7277df9a4bd7c724c1d7c1c7fc27be7f081e636
Steffen Geving <79884124+GEVIS1@users.noreply.github.com>
2024-01-17 17:55:05 +0000

    .

--- src/2023/python/10/main.py
+++ src/2023/python/10/main.py
@@ -58,38 +58,42 @@ def follow_path(tiles: list[str]) -> list[str]:
     path_distances[start_row][start_col] = str(steps_taken)
 
     while current_indices != start_indices or not ran_once:
-        for line in tiles:
-            print(line)
-        for line in path_distances:
-            print(line)
-
         ran_once = True
         possible_directions = find_possible_directions(tiles, current_indices)
 
         if len(possible_directions) == 1:
             col, row = possible_directions[0][2]
+
+            # TODO: This naive solution breaks on the actual input.
+            # We need a better way to determine if we are at the end.
             if tiles[row][col] == "S":
                 break
 
-        # print(f"I'm currently on the tile ({tiles[start_indices[1]][start_indices[0]]}) at col: {start_indices[1]} row: {start_indices[0]}")
-        # print("My possible directions are:")
         for direction, tile, indices in possible_directions:
             curr_col, curr_row = current_indices 
             move_col, move_row = indices
             already_visited_tile = path_distances[move_row][move_col] != "."
             
             if not already_visited_tile or indices == start_indices:
-                # print(f"Picking {direction} and moving to a \'{tile}\' tile.")
-                # print(f"This moves us from col: {curr_col} row: {curr_row} to col: {move_col} row: {move_row}.")
                 #calculated_distance = (abs(move_col - start_col) + abs(move_row - start_row))
-                # print(f"{calculated_distance=}")
                 steps_taken += 1
-                path_distances[move_row][move_col] = str(steps_taken) # TODO: Make int
-                print(f"Setting ({current_indices}) to ({indices})")
+                path_distances[move_row][move_col] = str(steps_taken) # TODO: Make int?
                 current_indices = indices
                 break
 
     return path_distances
+
+def path_distance_centering(path_distances: list[str]) -> list[str|int]:
+    max_distance = max(int(c) if c != "." else -1 for sub in path_distances for c in sub)
+    new_max_distance = max_distance // 2
+    new_path_distances = path_distances.copy()
+
+    for row, row_list in enumerate(new_path_distances):
+        for col, char in enumerate(row_list):
+            if (char != ".") and (char_as_int := int(char)) > new_max_distance:
+                new_path_distances[row][col] = char_as_int - new_max_distance
+
+    return new_path_distances
 
 def find_start(tiles: list[str]) -> tuple[int]:
     for y, row in enumerate(tiles):
@@ -117,10 +121,24 @@ def find_possible_directions(tiles: list[str], cur_pos: tuple[int, int]) -> list
 
     return possible_directions
 
+def find_max_distance(tiles: list[int|str]) -> int:
+    return max(n if isinstance(n, int) else -1 for sub in tiles for n in sub)
+
 if __name__ == "__main__":
     # Part 1 test
+    part1_test_expected_result = 8
     part1_test_input = load_input('./test.txt')
     
-    path_distances = follow_path(part1_test_input)
+    part1_test_path_distances = follow_path(part1_test_input)
 
-    
+    part1_test_formatted_distances = path_distance_centering(part1_test_path_distances)
+    part1_test_result = find_max_distance(part1_test_formatted_distances)
+    assert part1_test_result == part1_test_expected_result, f"Expected {part1_test_expected_result}, but got {part1_test_result}"
+
+    # Part 1 solution
+    part1_solution_input = load_input('./input.txt')
+    part1_solution_path_distances = follow_path(part1_solution_input)
+    part1_solution_formatted_distances = path_distance_centering(part1_solution_path_distances)
+    part1_solution_result = find_max_distance(part1_solution_formatted_distances)
+
+    print(f"Part 1 solution found: {part1_solution_result}")
